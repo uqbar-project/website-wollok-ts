688d1b535e502a41b7d37d99bc9266d51883cdbd
Fernando Dodino <fernando.dodino@gmail.com>
2024-09-01 10:35:49 -0300

    REPL: history & icon for evaluate

--- src/content/components/landing-page/repl/ReplEvaluator.tsx
+++ src/content/components/landing-page/repl/ReplEvaluator.tsx
@@ -1,5 +1,5 @@
 import './ReplEvaluator.css'
-import { useState, type KeyboardEvent } from 'react'
+import { useState, type ChangeEvent, type KeyboardEvent } from 'react'
 import { Package, link, Interpreter, Evaluation, WRENatives } from 'wollok-ts'
 
 const replPackage = new Package({ name: 'repl' })
@@ -9,12 +9,18 @@ new Interpreter(Evaluation.build(environment, WRENatives))
 export const ReplEvaluator = () => {
   const [result, setResult] = useState(0)
   const [resultClass, setResultClass] = useState('ok')
-  
+  const [expression, setExpression] = useState('')
+  const [history, setHistory] = useState<string[]>([])
+
+  console.info('history', history)
+
   const evaluate = () => {
     // @ts-ignore
-    console.info('el codigo es', document.getElementsByClassName('ace_text-layer')[0].innerText)
+    // console.info('el codigo es', document.getElementsByClassName('ace_text-layer')[0].innerText)
+    setHistory(history.concat(expression))
     setResult(Math.random() * 100)
     setResultClass('error')
+    setExpression('')
   }
 
   const keyDown = (event: KeyboardEvent<HTMLInputElement>) => {
@@ -23,10 +29,17 @@ export const ReplEvaluator = () => {
     }
   }
 
+  const expressionChanged = (event: ChangeEvent<HTMLInputElement>) => {
+    setExpression(event.target.value)
+  }
+
   return <section className="repl">
     <div className="line" id="editor">
-      <input type="text" className="expression" onKeyDown={keyDown}></input>
-      <button className="evaluate" onClick={() => evaluate()}>Evaluar</button>
+      <input type="text" className="expression" onKeyDown={keyDown} onChange={expressionChanged} value={expression}></input>
+      <button className="evaluate" onClick={() => evaluate()} title="Evaluar la expresión">
+        {/* https://github.com/feathericons/feather/blob/main/icons/alert-circle.svg */}
+        <img src="/src/assets/repl/evaluate.svg"/>
+      </button>
     </div>
     <div className={`result ${resultClass}`}>{result}</div>
   </section>
