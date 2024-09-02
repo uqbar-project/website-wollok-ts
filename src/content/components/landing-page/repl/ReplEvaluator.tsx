df1de61ccc5af7ab4cbf49290221747145c6b9bc
Fernando Dodino <fernando.dodino@gmail.com>
2024-09-02 00:21:43 -0300

    Changing layout & adding reload & refresh

--- src/content/components/landing-page/repl/ReplEvaluator.tsx
+++ src/content/components/landing-page/repl/ReplEvaluator.tsx
@@ -6,6 +6,7 @@ const replPackage = new Package({ name: 'repl' })
 const environment = link([replPackage])
 new Interpreter(Evaluation.build(environment, WRENatives))
 
+// TEMPORARY
 const results: Result[] = [
   { errored: false, result: 'true' },
   { errored: true, result: 'pepita does not understand volar' },
@@ -15,17 +16,7 @@ const results: Result[] = [
   { errored: false, result: '08/21/2024' },
 ]
 
-const generateResult = (expression: string, { errored, result }: { errored: boolean, result: string }) =>
-  !expression ?
-  <>
-    <div> </div>
-  </> :
-  <>
-    <div className="normal">{expression}</div>
-    <div className={errored ? 'error' :  'ok'}>
-      {errored ? '✗' : '✓'} {result}
-    </div>
-  </>
+const getResult = () => results[Math.trunc(Math.random() * results.length)]!
 
 // TODO: import from wollok-ts
 type Result = {
@@ -33,22 +24,30 @@ type Result = {
   error?: Error,
   result: string,
 }
+// END TEMPORARY
+
+const generateResult = (expression: string, { errored, result }: { errored: boolean, result: string }) =>
+  !expression ? 
+  undefined :
+  <div key={expression}>
+    <div className="normal">{expression}</div>
+    <div className={errored ? 'error' :  'ok'}>
+      {errored ? '✗' : '✓'} {result}
+    </div>
+  </div>
 
 export const ReplEvaluator = () => {
   const [expression, setExpression] = useState('')
-  const [oldExpression, setOldExpression] = useState('')
   const [history, setHistory] = useState<string[]>([])
   const [result, setResult] = useState<Result>({ errored: false, result: '' })
-  const fontFamily = document.getElementsByClassName('ace_content')
-
-  console.info(fontFamily)
+  const [formattedResult, setFormattedResult] = useState<JSX.Element | undefined>(undefined)
 
   const evaluate = () => {
     // @ts-ignore
     // console.info('el codigo es', document.getElementsByClassName('ace_text-layer')[0].innerText)
     setHistory(history.concat(expression))
+    setFormattedResult(generateResult(expression, result))
     setResult(results[Math.trunc(Math.random() * 6)]!)
-    setOldExpression(expression)
     setExpression('')
   }
 
@@ -63,6 +62,14 @@ export const ReplEvaluator = () => {
   }
 
   const reloadAndRefresh = () => {
+    const newResult = <div>
+      {
+      history.map((expression: string) =>
+        generateResult(expression, getResult()))
+      }
+    </div>
+    setFormattedResult(newResult)
+    setExpression('')
   }
 
   const reload = () => {}
@@ -75,16 +82,16 @@ export const ReplEvaluator = () => {
           {/* https://github.com/feathericons/feather/blob/main/icons */}
           <img src="/src/assets/repl/evaluate.svg"/>
         </button>
-        <button className="refresh" onClick={() => reloadAndRefresh()} title="Recarga el editor e inicia una nueva sesión del REPL">
+        <button className="refresh" onClick={() => reload()} title="Recarga el editor e inicia una nueva sesión del REPL">
           <img src="/src/assets/repl/refresh.svg"/>
         </button>
-        <button className="reload" onClick={() => reload()} title="Recarga el editor y ejecuta la última sesión activa">
+        <button className="reload" onClick={() => reloadAndRefresh()} title="Recarga el editor y ejecuta la última sesión activa">
           <img src="/src/assets/repl/reload.svg"/>
         </button>
       </div>
     </div>
-    <div className='result'>
-      {generateResult(oldExpression, result)}
-    </div>
+    {formattedResult && <div className='result'>
+      {formattedResult}
+    </div>}
   </section>
 }
